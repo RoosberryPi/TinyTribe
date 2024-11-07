ecb5bd41aeb04b5a76a669c17ebdb6c8e5a7081e
rosa.meijers <rosa.meijers@themobilecompany.com>
2024-11-07 13:01:11 +0100

    Initial Commit

---
+++ TinyTribe/TinyTribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTribeApp.swift
+//  TinyTribe
+//
+//  Created by rosa.meijers  on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TinyTribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
